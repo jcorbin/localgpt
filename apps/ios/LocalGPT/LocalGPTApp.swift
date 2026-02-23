cbd19dc74e7ff24752a413cd0a3a3d36a167c42d
Yi Wang <yi@yilab.com>
2026-02-22 21:50:47 -0500

    chore(ios): suppress UIKit Auto Layout warnings in debug
    iPad keyboard handling triggers benign constraint conflicts in
    _UIRemoteKeyboardPlaceholderView. Suppress these known Apple bugs
    in debug builds only.

--- apps/ios/LocalGPT/LocalGPTApp.swift
+++ apps/ios/LocalGPT/LocalGPTApp.swift
@@ -3,6 +3,14 @@ import LocalGPTWrapper
 
 @main
 struct LocalGPTApp: App {
+    init() {
+        #if DEBUG
+        // Suppress UIKit's internal Auto Layout constraint warnings on iPad
+        // These are known Apple bugs in _UIRemoteKeyboardPlaceholderView
+        UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             ChatView()
